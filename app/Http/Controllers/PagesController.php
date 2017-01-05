d08cb4107f65bbb04cd4f2caa0e6b399f7b0ce0e
finpickford <u1362372@unimail.hud.ac.uk>
2017-01-05 16:24:24 +0000

    Added basic page with search bar

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// use Illuminate\Foundation\Bus\DispatchesJobs;
+// use Illuminate\Routing\Controller as BaseController;
+// use Illuminate\Foundation\Validation\ValidatesRequests;
+// use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class PagesController extends Controller
+{
+    // use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function home(){
+
+          $watches  = ['Rolex', 'Omega'];
+          return view('welcome', compact('watches'));
+      }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,34 @@
 
 @section('content')
 
-      <div class="content">
-          <div class="title m-b-md">
-              <p>Welcome to WristWatch, a place to search and research wrist watches.<p>
-              
-          </div>
-      </div>
+
+              <h1>Welcome to WristWatch, a place to search and research wrist watches.<h1>
+
+              <p>
+              Use the search box below to search for a watch.
+              </p>
+
+              @unless (empty($watches))
+                <p>There are some watches:</p>
+
+              @else
+                <p>There are no watches.</p>
+
+              @endunless
+
+<form action=""> Search:
+  <input type="search" name="watchsearch">
+  <input type="submit">
+</form>
+
+                @foreach ($watches as $watch)
+
+                    <li> {{ $watch }} </li>
+
+                @endforeach
 
 @endsection
+
+@section('footer')
+
+@stop
