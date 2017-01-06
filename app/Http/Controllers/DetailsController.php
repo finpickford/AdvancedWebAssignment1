cd1a82e6a49e138040e9d9a11ec941557b698f66
finpickford <u1362372@unimail.hud.ac.uk>
2017-01-06 22:05:02 +0000

    Last commit of the day

--- app/Http/Controllers/DetailsController.php
+++ app/Http/Controllers/DetailsController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers;
 
-
 use Illuminate\Http\Request;
+
+use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 class DetailsController extends Controller
 {
-public function store() {
+  public function store() {
 
-  return $request()->all();
+    return $request()->all();
 
-}
+  }
 }
